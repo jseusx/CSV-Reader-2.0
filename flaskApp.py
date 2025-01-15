b17a04ff858bf012836938a682958a3b33a2fbdc
Jesus Jimenez <jesusjimenez.mail@gmail.com>
2025-01-15 16:21:24 -0500

    Added functions for reading and creating db table.

--- flaskApp.py
+++ flaskApp.py
@@ -1,12 +1,52 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+import sqlite3
+import csv
 
 app = Flask(__name__)
+
+def init_db():
+    with sqlite3.connect('funding_database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS funding_information (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       serial_number TEXT NOT NULL,
+                       model TEXT NOT NULL,
+                       order_number TEXT,
+                       funding_source TEXT
+                       )
+                       ''')    
+        conn.commit()
+
+def import_csv_to_db(csv_file_path,db_path):
+    # connect to db
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # open csv file
+    with open(csv_file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+
+        for row in csv_reader:
+            # insert data into database
+            cursor.execute('''
+                INSERT INTO funding_information (serial_number, model, order_number, funding_source)
+                values (?, ?, ?, ?)
+                ''', (row['S/N'], row['model'], row['order_number'], row['funding']))
+            
+        conn.commit()
+        conn.close()
+    print("Data imported successfully!")
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         serial_number = request.form.get("serial_number")
         # Add logic to process serial number
+
+
         return f"Serial Number Submitted: {serial_number}"
     return render_template("index.html")
 
